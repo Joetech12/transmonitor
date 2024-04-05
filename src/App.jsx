c3e653f607d16c42be8a4053b8c7c88c5132362b
Joetech12 <ifeanyi4umeh@gmail.com>
2024-04-05 16:13:05 +0100

    Initial project commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className="text-3xl text-red-500">Hello</h1>
+    </>
+  )
+}
+
+export default App
